04153f86354a29e05273e05c5c552654e876bc40
Sergey Aleksandrov <a_svl@yahoo.com>
2014-03-14 15:00:07 +0400

    test

--- www/pages/main_page.js
+++ www/pages/main_page.js
@@ -1,7 +1,7 @@
 var mainPage_fb = new sap.m.FlexBox({
                                items: [
                                  new sap.m.Button({
-                                    text: "Ввести затрату",
+                                    text: "Ввести затрату ",
                                     press: function() {
                                                       sapUiApp.to("expense_card");
                                                   }
